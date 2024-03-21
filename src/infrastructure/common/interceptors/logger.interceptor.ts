630f9ee314cb1886d2c74551f69a48f42cbe19c5
Arnaud <arnaud.roche.26@gmail.com>
2024-03-21 12:20:50 +0100

    add interceptors logge, response - add helmet plugin

---
+++ src/infrastructure/common/interceptors/logger.interceptor.ts
@@ -0,0 +1,46 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+import { LoggerService } from 'src/infrastructure/logger/logger.service';
+
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  constructor(private readonly logger: LoggerService) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+    const httpContext = context.switchToHttp();
+    const request = httpContext.getRequest();
+    const ip = this.getIp(request);
+    this.logger.log(
+      `Request incoming on ${request.path}`,
+      `method=${request.method} ip=${ip}`,
+    );
+    return next.handle().pipe(
+      tap(() => {
+        this.logger.log(
+          `End Request for ${request.path}`,
+          `method=${request.method} ip=${ip} now=${now}`,
+        );
+      }),
+    );
+  }
+
+  private getIp(request: any): string {
+    let ip: string;
+    const ipAddr = request.headers['x-forwarded-for'];
+    if (ipAddr) {
+      const list = ipAddr.split(',');
+      ip = list[list.length - 1];
+      console.log(ip);
+    } else {
+      ip = request.connection.remoteAddress;
+    }
+    ip.replace('::fff:', '');
+    console.log(ip);
+    return ip;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import {
 } from '@nestjs/platform-fastify';
 import { documentSwagger } from './infrastructure/common/swagger/swagger';
 import compression from '@fastify/compress';
+import helmet from '@fastify/helmet';
 import { AllExceptionFilters } from './infrastructure/common/exception-filters-middleware/all-exception-filters.middleware';
+import { LoggerInterceptor } from './infrastructure/common/interceptors/logger.interceptor';
+import { ResponseInterceptor } from './infrastructure/common/interceptors/response.interceptor';
+import { LoggerService } from './infrastructure/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -16,8 +20,13 @@ async function bootstrap() {
 
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionFilters(httpAdapter));
+  app.useGlobalInterceptors(
+    new LoggerInterceptor(new LoggerService()),
+    new ResponseInterceptor(),
+  );
 
   await app.register(compression);
+  await app.register(helmet);
   documentSwagger(app);
   await app.listen(3000);
 }
